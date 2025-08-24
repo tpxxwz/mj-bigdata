eccd3ff7a57bc4db50017e093ebd063b78f265bd
wjj <tpxxwz@gmail.com>
2025-08-25 08:50:20 +0900

    sql first temp

---
+++ mj-flink-basic/src/main/java/com/mj/sql/hello/Hello2APIDDL.java
@@ -0,0 +1,28 @@
+package com.mj.sql.hello;
+
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+
+public class Hello2APIDDL {
+    public static void main(String[] args) {
+        // 运行环境
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(environment);
+        // 创建表：方案1
+        FileSource<String> source = FileSource
+                .forRecordStreamFormat(new TextLineInputFormat(), new Path("file:///tmp/dept.txt"))
+                .build();
+        DataStreamSource<String> deptSource = environment.fromSource(
+                source,
+                org.apache.flink.api.common.eventtime.WatermarkStrategy.noWatermarks(),
+                "file-source");
+        Table deptTable = tableEnvironment.fromDataStream(deptSource);
+        deptTable.execute().print();
+    }
+}

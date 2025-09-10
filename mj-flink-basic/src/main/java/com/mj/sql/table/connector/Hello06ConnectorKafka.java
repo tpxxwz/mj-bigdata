963f46dfa5693750798df9d5e0a8be09eacb424f
zhe.wang <zhe.wang@okcoin.jp>
2025-09-10 19:25:45 +0900

    更新

--- mj-flink-basic/src/main/java/com/mj/sql/table/connector/Hello06ConnectorKafka.java
+++ mj-flink-basic/src/main/java/com/mj/sql/table/connector/Hello06ConnectorKafka.java
@@ -1,9 +1,48 @@
 package com.mj.sql.table.connector;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 public class Hello06ConnectorKafka {
 
     public static void main(String[] args) {
+        // 执行环境
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(environment);
+        // 创建表：source
+        tableEnvironment.executeSql("""
+                CREATE TABLE flink_source_table (
+                `deptno` INT,
+                `dname` STRING,
+                `loc` STRING
+                ) WITH (
+                'connector' = 'kafka',
+                'topic' = 'kafka_source_topic',
+                'properties.bootstrap.servers' = 'mj01:6667',
+                'properties.group.id' = 'yjxxtliyi',
+                'scan.startup.mode' = 'earliest-offset',
+                'format' = 'csv'
+                )
+                """);
 
+        // 创建表：SINK
+        tableEnvironment.executeSql("""
+                CREATE TABLE flink_sink_table (
+                `deptno` INT,
+                `dname` STRING,
+                `loc` STRING
+                ) WITH (
+                'connector' = 'kafka',
+                'topic' = 'kafka_sink_topic',
+                'properties.bootstrap.servers' = 'mj01:6667',
+                'properties.group.id' = 'yjxxtliyi',
+                'scan.startup.mode' = 'earliest-offset',
+                'format' = 'json'
+                )
+                """);
+        // 运行代码
+        // tableEnvironment.sqlQuery("select * from flink_sink_table").execute().print();
+        tableEnvironment.sqlQuery("select * from flink_source_table").insertInto("flink_sink_table").execute();
     }
 
 }
--- mj-flink-basic/src/main/java/com/mj/sql/table/connector/Hello09ConnectorKafkaUpsert.java
+++ mj-flink-basic/src/main/java/com/mj/sql/table/connector/Hello09ConnectorKafkaUpsert.java
@@ -1,11 +1,13 @@
 package com.mj.sql.table.connector;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 
 public class Hello09ConnectorKafkaUpsert {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // 运行环境
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
         environment.setParallelism(1);
@@ -19,15 +21,14 @@ public class Hello09ConnectorKafkaUpsert {
                   WATERMARK FOR ts AS ts
                 ) WITH (
                    'connector' = 'datagen',
-                   'rows-per-second' = '2',
+                   'rows-per-second' = '1',
                    'fields.deptno.min' = '88',
                    'fields.deptno.max' = '99',
                    'fields.salenum.min' = '1',
                    'fields.salenum.max' = '9'
                 )""");
-        // 查询部门销售详情
-        // tableEnvironment.sqlQuery("select deptno, sum(salenum) as sumsale from t_dept group by deptno").execute().print();
-
+        TableResult tableResult1 = tableEnvironment.from("t_dept").execute();
+        new Thread(tableResult1::print).start();
         // 插入到一张Kafka的表中
         tableEnvironment.executeSql("""
                 CREATE TABLE flink_dept_sale_sum (
@@ -42,20 +43,12 @@ public class Hello09ConnectorKafkaUpsert {
                   'value.format' = 'json'
                 );
                 """);
-        tableEnvironment.executeSql("""
-                CREATE TABLE flink_dept_sale_sum_for_check (
-                  deptno INT,
-                  sumsale INT
-                ) WITH (
-                  'connector' = 'kafka',
-                  'topic' = 'topic_dept_sale_sum',
-                  'properties.bootstrap.servers' = 'mj01:6667',
-                  'format' = 'json'
-                );
-                """);
-        new Thread(() -> tableEnvironment.executeSql("select * from flink_dept_sale_sum_for_check").print()).start();
+        // tableEnvironment.sqlQuery("select * from flink_dept_sale_sum_for_check").execute().print();
+        TableResult tableResult2 = tableEnvironment.from("flink_dept_sale_sum").execute();
+        new Thread(tableResult2::print).start();
         // 插入数据
         tableEnvironment.executeSql("insert into flink_dept_sale_sum select deptno, sum(salenum) as sumsale from t_dept group by deptno");
+
     }
 
 }
